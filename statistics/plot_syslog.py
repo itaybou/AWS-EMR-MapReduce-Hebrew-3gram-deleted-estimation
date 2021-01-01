c1582fa161c61107e121598f87c2b273048136e5
itay <itaibu@gmail.com>
2021-01-02 01:30:20 +0200

    Updated python plot script

--- statistics/plot_syslog.py
+++ statistics/plot_syslog.py
@@ -9,7 +9,7 @@ jobs = ['Split Corpus', 'Aggregate Nr', 'Aggregate Tr', 'Join Nr Tr 3grams', 'Ca
 statistics = ['Map input records', 'Map output records', 'Combine input records', 'Combine output records', 'Reduce input records', 'Reduce output records']
 counters = ['NGRAMS_COUNTER']
 
-input_files = glob(join('./statistics/', 'syslog*'))
+input_files = glob(join('.', 'syslog*'))
 
 stats_dict = {}
 
@@ -33,14 +33,14 @@ df.columns = ['Status', 'Statistic', 'Stage', 'Value']
 def display_figures(ax):
    stats_index = 0
    for i, p in enumerate(ax.patches):
-      if (i + 1) % len(statistics) == 0:
-         stats_index = (stats_index + 1) % len(statistics)
       width = p.get_width()    # get bar length
       ax.text(width + 1,       # set the text at 1 unit right of the bar
                p.get_y() + p.get_height() / 2, # get Y coordinate + X coordinate / 2
                f'{statistics[stats_index]}: {int(width)}', # set variable to display, 2 decimals
                ha = 'left',   # horizontal alignment
                va = 'center')  # vertical alignment
+      if (i + 1) % len(statistics) == 0:
+         stats_index = (stats_index + 1) % len(statistics)
    plt.show()
 
 combiner_stats_df = df.loc[df['Status'] == 'Combiner']
